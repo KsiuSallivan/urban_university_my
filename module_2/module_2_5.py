d79e515a8b39d38d95acc88b4384a797f72e52fa
KsiuSallivan <ksiu.sallivan@gmail.com>
2024-08-26 17:50:09 +0300

    module_2_5

--- module_2/module_2_5.py
+++ module_2/module_2_5.py
@@ -2,10 +2,8 @@ def get_matrix (n, m, value):
     matrix = []
     for i in range(n):
         matrix.append([])
-        for j in matrix:
-            j = [value] * m
-            matrix = [j] * n
-            break
+        for j in range(m):
+            matrix[i].append(value)
     return matrix
 
 
