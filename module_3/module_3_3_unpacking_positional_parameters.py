607325cf72707de026a35b581052102b0945720d
KsiuSallivan <ksiu.sallivan@gmail.com>
2024-09-01 16:47:36 +0300

    module_3_3

---
+++ module_3/module_3_3_unpacking_positional_parameters.py
@@ -0,0 +1,18 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+print_params() # вывод: 1 строка True
+print_params(b=25) # вывод: 1 25 True
+print_params(c=[1,2,3]) # вывод: 1 строка [1, 2, 3]
+
+
+values_list = ['difficult', 25, True]
+values_dict = {'a': '1', 'b': 'строка', 'c': 'True'}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+
+values_list_2 = ['stress', 100500]
+
+print_params(*values_list_2, 42)
