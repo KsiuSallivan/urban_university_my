2b879d2ef0974cb008daaee49c88c53cc14ff498
KsiuSallivan <ksiu.sallivan@gmail.com>
2024-10-08 12:04:36 +0300

    7-4

---
+++ module_7/module_7_4_string_format.py
@@ -0,0 +1,26 @@
+# Использование %:
+
+team1_num = 5
+team2_num = 6
+
+print('В команде Мастера кода участников: %s' % team1_num + '!')
+print('Итого сегодня в командах участников: %s ' % team1_num + 'и %s' % team2_num + '!')
+
+# Использование format():
+
+score_1 = 40
+score_2 = 42
+team1_time = 1552.512
+team2_time = 2153.31451
+tasks_total = 82
+time_avg = 45.2
+challenge_result = 'Мастера кода'
+
+print('Команда Волшебники данных решила задач: {}!'.format(score_2))
+print('Волшебники данных решили задачи за {}'.format(team1_time) + ' c !')
+
+# Использование f-строк:
+
+print(f'Команды решили {score_1} и {score_2} задач.')
+print(f'Результат битвы: победа команды {challenge_result}!')
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!')
