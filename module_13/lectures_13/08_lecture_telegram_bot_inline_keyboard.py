87a6c20640979e3028a0877646860d47587183cf
KsiuSallivan <40688933+KsiuSallivan@users.noreply.github.com>
2024-11-05 13:55:49 +0300

    Add files via upload

---
+++ module_13/lectures_13/08_lecture_telegram_bot_inline_keyboard.py
@@ -0,0 +1,41 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+api = "7733_my_token"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+kb = InlineKeyboardMarkup()
+button = InlineKeyboardButton(text='Информация', callback_data='info')
+kb.add(button)
+
+start_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='info')],
+        [
+            KeyboardButton(text='shop'),
+            KeyboardButton(text='donate')
+        ],
+    ],
+    resize_keyboard=True
+)
+
+
+@dp.message_handler(commands=['start'])
+async def starter(message):
+    await message.answer('Рады вас видеть!', reply_markup=start_menu)
+
+
+# @dp.callback_query_handler(text='info')
+# async def infor(call):
+#     await call.message.answer('Информация о боте!')
+#     await call.answer()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
---
+++ module_13/module_13_6_telegram_bot_more_choices.py
@@ -0,0 +1,85 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+
+api = "7733_my_token"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+# Группа состояний
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+# Клавиатура
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard.add(KeyboardButton('Рассчитать'), KeyboardButton('Информация'))
+
+# Inline-клавиатура
+inline_keyboard = InlineKeyboardMarkup()
+inline_keyboard.add(InlineKeyboardButton('Рассчитать норму калорий', callback_data='calories'))
+inline_keyboard.add(InlineKeyboardButton('Формулы расчёта', callback_data='formulas'))
+
+
+# Функция для старта
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer('Привет! Выберите действие:', reply_markup=keyboard)
+
+
+# Функция для вывода Inline-меню
+@dp.message_handler(text='Рассчитать')
+async def main_menu(message: types.Message):
+    await message.answer('Выберите опцию:', reply_markup=inline_keyboard)
+
+
+# Функция для вывода формулы
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call: types.CallbackQuery):
+    await call.message.answer('Формула Миффлина-Сан Жеора:\nБМР = 10 * вес + 6.25 * рост - 5 * возраст - 161')
+    await call.answer()
+
+
+# Функция для установки возраста
+@dp.callback_query_handler(text='calories')
+async def set_age(call: types.CallbackQuery):
+    await call.message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+# Функция для установки роста
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+
+# Функция для установки веса
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+
+# Функция для расчета и отправки калорий
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message: types.Message, state: FSMContext):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+
+    # Упрощенная формула Миффлина - Сан Жеора для женщин
+    bmr = 10 * float(data['weight']) + 6.25 * float(data['growth']) - 5 * float(data['age']) - 161
+    await message.answer(f'Ваша норма калорий: {bmr:.2f} ккал')
+
+    await state.finish()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
